bbdeaea06765f52d0d5ce55ebe0a082532004bb7
Yordan <yordanmarinsovero@gmail.com>
2025-04-10 09:09:03 -0500

    lista circular y doble

---
+++ ejercicios/clases/NodoDoble.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios.clases
+{
+    internal class NodoDoble
+    {
+        private int numero;
+        private NodoDoble siguiente;
+        private NodoDoble atras;
+
+        public int Numero { get => numero; set => numero = value; }
+        internal NodoDoble Siguiente { get => siguiente; set => siguiente = value; }
+        internal NodoDoble Atras { get => atras; set => atras = value; }
+    }
+}
